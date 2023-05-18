6b03911382dcb757394b6760ef35a1036e9e90ee
espadane <espadane@gmail.com>
2023-05-18 11:13:13 +0300

    Fixed the display issue in the admin panel

--- products_accounting/products/admin.py
+++ products_accounting/products/admin.py
@@ -2,7 +2,29 @@ from django.contrib import admin
 from .models import Product, Store, Inventory, Transfer
 
 
-admin.site.register(Product)
-admin.site.register(Store)
-admin.site.register(Inventory)
-admin.site.register(Transfer)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+    ordering = ['name']
+
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+    ordering = ['name']
+    
+@admin.register(Inventory)
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ['product', 'store', 'quantity']
+    list_filter = ['store']
+    search_fields = ['product__name']
+    ordering = ['store', 'product']
+    
+@admin.register(Transfer)
+class TransferAdmin(admin.ModelAdmin):
+    list_display = ['product', 'store', 'difference', 'date', 'user', 'comment']
+    list_filter = ['store', 'date']
+    date_hierarchy = 'date'
+    search_fields = ['product__name']
+    ordering = ['date', 'product']
